6fda256b16bbf76345bace53bf6acf4ceaebffe5
begli <lxwslduck@gmail.com>
2025-11-16 18:29:57 +0500

    added sorting of files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,22 @@ import Main from './components/Main'
 import PathContext from './store/path-context-creator.js'
 import SearhInput from './components/SearchInput'
 import Dropdown from './components/Dropdown.jsx';
+import classes from './App.module.css'
 
 function App() {
+  const [organizeState, setOrganizeState] = useState('name-sort');
+  const [typeState, setTypeState] = useState('ascending');
+
+  function handleChoose(e) {
+    setOrganizeState(e.target.value)
+    console.log(e.target.value);
+  }
+
+  function handleType(e) {
+    setTypeState(e.target.value);
+    console.log(e.target.value)
+  }
+
   const [isFocus, setIsFocus] = useState(false);
   const { files, path, currentPath } = useContext(PathContext);
 
@@ -13,6 +27,26 @@ function App() {
   const filteredFolders = suggestions.filter((folder) =>
     (currentPath + folder).toLowerCase().startsWith(path.toLowerCase())
   );
+
+  if(organizeState === 'name-sort') {
+    if(typeState === 'descending') {
+      files.folders.sort((a, b) => b.name.localeCompare(a.name));
+      files.documents.sort((a, b) => b.name.localeCompare(a.name));
+    } else {
+      files.folders.sort((a, b) => a.name.localeCompare(b.name));
+      files.documents.sort((a, b) => a.name.localeCompare(b.name));
+    }
+  }
+
+  if(organizeState === 'date-sort') {
+    if(typeState === 'descending') {
+      files.folders.sort((a, b) => b.modified - a.modified);
+      files.documents.sort((a, b) => b.modified - a.modified);
+    } else {
+      files.folders.sort((a, b) => a.modified - b.modified);
+      files.documents.sort((a, b) => a.modified - b.modified);
+    }
+  }
 
   return (
     <div style={{
@@ -28,6 +62,31 @@ function App() {
         /></summary>
         <Dropdown folders={filteredFolders}/>
       </details>
+
+      <form className={classes.form}>
+        <div>
+          <label htmlFor="organize">Organize: </label>
+          <select onChange={handleChoose} value={organizeState} className={classes.select} name="organize" id="">
+            <optgroup className={classes.optgroup} label='Sort by'>
+              <option className={classes.option} value="name-sort">Name</option>
+              <option className={classes.option} value="date-sort">Date modified</option>
+            </optgroup>
+            <hr />
+            <optgroup className={classes.optgroup} label='Group by'>
+              <option className={classes.option} value="name-group">Name</option>
+              <option className={classes.option} value="date-group">Date modified</option>
+            </optgroup>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="type">Type: </label>
+          <select onChange={handleType} value={typeState} className={classes.select} name="type" id="">
+            <option className={classes.option} value="ascending">Ascending</option>
+            <option className={classes.option} value="descending">Descending</option>
+          </select>
+        </div>
+      </form>
+
       <Main items={files}/>
     </div>
   )
